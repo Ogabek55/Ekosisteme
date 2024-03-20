9107bc9d2bd71b6c0ebe5686d1eea338b7da8973
Shamshiyev Og'abek <shamshiyevogabek5555@gmail.com>
2024-03-20 10:32:15 +0500

    add

---
+++ jscript.js
@@ -0,0 +1,17 @@
+// let name = prompt("Assalomu alaykum Hurmatli mijoz Ismingiz va Familyangiz ");
+// let name1 = prompt("Kasbinggiz");
+// let name2 = prompt("Yoshingiz ");
+// let name3 = prompt("Telefon raqamingiz ");
+// let name4 = prompt("Yashash joyingiz haqida to'liq malumot qoldiring ");
+// alert(`Ismingiz, ${name}`)
+// alert(`Sizning Kasbingiz, ${name1}`)
+// alert(`Yoshingiz, ${name2}`)
+// alert(`Telefon raqamingiz, ${name3}`)
+// alert(`yashash joyingiz, ${name4}`)
+// confirm(`Hamma ma'lumotlaringiz to'g'riligiga ishonchingiz komilmi!`)
+// document.write(`${name},`);
+// document.write(` ${name1},`);
+// document.write(` ${name2},`);
+// document.write(` ${name3},`);
+// document.write(`${name4},`);
+
